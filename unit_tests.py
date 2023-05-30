030ef4a5b3a0a8132c882582e3ff38d3d96adfb2
Ethan Berrett <ethanb@qualtrics.com>
2023-05-29 19:28:55 -0500

    uncommented crypto unit test

--- unit_tests.py
+++ unit_tests.py
@@ -71,25 +71,25 @@ class TestRetrievalMethods(unittest.TestCase):
         print(sell_results)
         self.assertIsInstance(sell_results, list)
 
-# class TestCryptoRetrievalMethods(TestRetrievalMethods):
+class TestCryptoRetrievalMethods(TestRetrievalMethods):
 
-#     def __init__(self, methodName: str = "runTest") -> None:
-#         super().__init__(methodName)
-#         self.robin_bot = RobinCryptoBot(**example_config.config, sandbox=True)
-#     test_symbol = 'BTC'
-#     test_symbol_list = example_config.config["crypto_watchlist"]
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.robin_bot = RobinCryptoBot(**example_config.config, sandbox=True)
+    test_symbol = 'BTC'
+    test_symbol_list = example_config.config["crypto_watchlist"]
 
-#     def test_get_top_n_stocks(self):
-#         pass
+    def test_get_top_n_stocks(self):
+        pass
 
-#     def test_buy_from_top_stocks(self):
-#         pass
+    def test_buy_from_top_stocks(self):
+        pass
 
-#     def test_buy_from_list(self):
-#         purchase_results = self.robin_bot.buy_from_ticker_list(
-#             self.test_symbol_list)
-#         print(purchase_results)
-#         self.assertIsInstance(purchase_results, list)
+    def test_buy_from_list(self):
+        purchase_results = self.robin_bot.buy_from_ticker_list(
+            self.test_symbol_list)
+        print(purchase_results)
+        self.assertIsInstance(purchase_results, list)
 
 
 if __name__ == '__main__':
